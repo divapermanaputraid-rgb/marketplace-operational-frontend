1696b71ae0f55548e5be99d2760da1140b2fccc5
divapermanaputraid-rgb <divapermanaputra.id@gmail.com>
2026-05-13 21:34:14 +0700

    chore(frontend): remove generated declaration files

--- src/components/integrations/PullOrdersModal.d.ts
+++
@@ -1,8 +0,0 @@
-interface PullOrdersModalProps {
-    storeId: string;
-    storeName: string;
-    isOpen: boolean;
-    onClose: () => void;
-}
-export declare function PullOrdersModal({ storeId, storeName, isOpen, onClose }: PullOrdersModalProps): import("react/jsx-runtime").JSX.Element | null;
-export {};
